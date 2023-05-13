db1b837b471b5d5e0d387b63db2aa687afe27547
ququzone <xueping.yang@gmail.com>
2023-05-13 22:04:22 +0800

    dkim keys

--- src/DkimKeys.sol
+++ src/DkimKeys.sol
@@ -4,7 +4,27 @@ pragma solidity ^0.8.13;
 import "solmate/auth/Owned.sol";
 
 contract DkimKeys is Owned {
+    event UpdateDKIMKey(bytes server, bytes key);
+    event DeleteDKIMKey(bytes server, bytes key);
+
     mapping(bytes => bytes) private dkimKeys;
 
     constructor() Owned(msg.sender) {}
+
+    function updateKey(bytes memory server, bytes memory key) external onlyOwner {
+        dkimKeys[server] = key;
+        emit UpdateDKIMKey(server, key);
+    }
+
+    function removeKey(bytes memory server) external onlyOwner {
+        bytes memory key = dkimKeys[server];
+        delete dkimKeys[server];
+        emit DeleteDKIMKey(server, key);
+    }
+
+    function getKey(bytes memory server) external view returns (bytes memory) {
+        bytes memory key = dkimKeys[server];
+        require(key.length > 0, "no key");
+        return key;
+    }
 }
