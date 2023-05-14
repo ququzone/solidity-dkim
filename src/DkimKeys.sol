5cfb98026c379639af996eb275f7c3080b353a8e
ququzone <xueping.yang@gmail.com>
2023-05-14 10:40:43 +0800

    change server to bytes32

--- src/DkimKeys.sol
+++ src/DkimKeys.sol
@@ -4,25 +4,25 @@ pragma solidity ^0.8.13;
 import "solmate/auth/Owned.sol";
 
 contract DkimKeys is Owned {
-    event UpdateDKIMKey(bytes server, bytes key);
-    event DeleteDKIMKey(bytes server, bytes key);
+    event UpdateDKIMKey(bytes32 server, bytes key);
+    event DeleteDKIMKey(bytes32 server, bytes key);
 
-    mapping(bytes => bytes) private dkimKeys;
+    mapping(bytes32 => bytes) private dkimKeys;
 
     constructor() Owned(msg.sender) {}
 
-    function updateKey(bytes memory server, bytes memory key) external onlyOwner {
+    function updateKey(bytes32 server, bytes memory key) external onlyOwner {
         dkimKeys[server] = key;
         emit UpdateDKIMKey(server, key);
     }
 
-    function removeKey(bytes memory server) external onlyOwner {
+    function removeKey(bytes32 server) external onlyOwner {
         bytes memory key = dkimKeys[server];
         delete dkimKeys[server];
         emit DeleteDKIMKey(server, key);
     }
 
-    function getKey(bytes memory server) external view returns (bytes memory) {
+    function getKey(bytes32 server) external view returns (bytes memory) {
         bytes memory key = dkimKeys[server];
         require(key.length > 0, "no key");
         return key;
